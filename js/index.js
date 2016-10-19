7eaff655092eed1d06379bcfcfdbabfb603fabcc
bseelig <Guest@BSDs-iMac-5.local>
2016-10-19 17:02:37 -0400

    -

---
+++ js/index.js
@@ -0,0 +1,6 @@
+var answer;
+
+function go() {
+  alert('This Web site works by Github. Github is a program that lets you make your own site and gets changed by the program sublime text. You can write the code in codepen to see the changes that you made.');
+  answer = prompt; 
+}
