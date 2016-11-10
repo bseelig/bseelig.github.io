a9d9703bcb7485dcc48726948a8d3f8907add217
bseelig <bseelig@me.com>
2016-11-09 21:39:02 -0500

    Test Added and Dark Sites

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
+alert('Welcome to the electronic repair web site. To start, Click on the toolbar to find and fix your problem.');
+
 var answer;
 
 function go() {
-  alert('This Web site works by Github. Github is a program that lets you make your own site and gets changed by the program sublime text. You can write the code in codepen to see the changes that you made.');
+  alert('');
   answer = prompt; 
 }
