5a4e15e196d1b092d722b4a75aefc4b3da6914af
bseelig <bseelig@me.com>
2017-02-23 02:41:03 -0500

    No More Message

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-alert('Welcome to the electronic repair web site. To start, Click on the toolbar to find and fix your problem.');
+//alert('Welcome to the electronic repair web site. To start, Click on the toolbar to find and fix your problem.');
 
 var answer;
 
