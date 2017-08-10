dc3ddb4b2020228865c7bcc12406d5bb703d2167
bseelig365code <Brett@Bretts-MBP.fios-router.home>
2017-08-10 18:45:06 -0400

    New text improvements + Photos

---
+++ Computers/Apple/js/index.js
@@ -0,0 +1,6 @@
+function repair() {
+  alert("Hold the power button until the computer turns off. Then as you turn on the computer, hold the Command R keys until the screen shows an apple logo or lock. If its a lock, enter the admin or master password. (Warning this bootup will take longer than a normal boot)");
+  alert("After it boots up, click on 'Disk Utility.' In the Internal section, click on the top hard drive and then click 'First Aid' then 'Run.' (This will only take a few mins) Do the same thing clicking on 'Macintosh HD' from step one. If the 'First Aid' button is locked click on the 'Mount' button and try again.")
+  alert("When you are done, click on the apple logo on the top left side then click restart.")
+}
+
--- Computers/Windows/startup/js/index.js
+++ Computers/Windows/startup/js/index.js
@@ -12,19 +12,18 @@ if( answer == true ){
    alert("Go to the Advanced section then look for Stata Configuration. Then change the Stata Mode to a differnt setting. If you cannot find it, look all around the bios settings. Then exit saving changes and try booting up again.");
 }
   else{
-    alert("Exiting Steps");
+    alert("Hold the power button until the computer powers off. Then press the power button to turn it on. When you see the screen popup, look around for a text ('menu' or 'setup') and then press and hold on that key to go to the setup. If the screen disappers to fast, use a video camra or phone camra to replay. Pause at the screen right when it turns on. (To start this, restart to step one on this text).  If its a menu, press on the 'F (number)' key (On the top of the keybord) on the text 'BIOS Settings' or 'BIOS Setup'");
+    alert("Go to the Advanced section then look for Stata Configuration. Then change the Stata Mode to a differnt setting. If you cannot find it, look all around the bios settings. Then exit saving changes and try booting up again.");
   }
 }
 }
 
 function em() {
-  var answer = confirm("You will need a Windows Installer CD and a flash drive inorder to fix your problem. (This is for Windows 7) If not, you will lose all data and cannot continue in the steps.");
+  var answer = confirm("You will need a Windows Installer CD and a flash drive inorder to fix your problem. (This is for Windows 7) If not, you will lose all data and cannot continue the steps.");
   
   if( answer == true ){
-   alert("Turn on your computer. Then boot into the CD. Select you language and then click Repair your computer.");
-    alert("Now click the next step and you will see repairing options. Click on the System Image Recovery");
-     alert("Error message will come up. Click Cancel then  Next then Advanced then Install a driver then OK");
-    alert("Dobble Click on the computer. Now Plug in your flash drive. ");
+   alert("Turn on your computer. Then boot into the CD. Select you language and then click Repair your computer. If its not booting into CD, Hold the power button until the computer powers off and then turn it back on. Hold the F12 key for the bootup menu use the keybord to move highlighted text and press enter to select.");
+    alert("Now click the next step and you will see repairing options. Click on the System Image Recovery. Error message will come up. Click Cancel then  Next then Advanced then Install a driver then OK. Dobble Click on the computer. Then Plug in your flash drive.");
     alert("Now click on the Local Disk and then click users and coppy all your data into the flash drive. Big files must be coppyed separately in one coppy. To enter into your flash drive, Click computer then your flash drive. You must right click to coppy and paste.");
   alert("When you are done, Reinstall Windows.");
   }
@@ -41,7 +40,7 @@ function ns() {
    alert("Click on the 'Blue Screen' button to Continue.");
 }
     else {
-      alert("Exiting")
+      alert("Click on 'Error message' to continue.")
     }
 }
 else {
@@ -51,7 +50,6 @@ else {
 }
 
 function ss() {
-  alert("Hold the power button until the computer turns off. Then turn it back on.");
-  alert("Keep pressing the F8 button rapidly until you see a menu. Use the keybord arrows to move the highlighted text. Move the highlight text to the 'Last Known Good Configuration (advanced).' Then press enter");
-  alert("If it dose not work restart to step one but instead of doing 'Last Known Good Configuration (advanced)' Just press enter on repair your computer. Then Click on startup repair and if it asks for system restore click restore.");
+  alert("Hold the power button until the computer turns off. Then turn it back on. Keep pressing the F8 button rapidly until you see a menu. Use the keybord arrows to move the highlighted text. Move the highlight text to the 'Last Known Good Configuration (advanced).' Then press enter");
+  alert("If it dose not work restart to step one but instead of doing 'Last Known Good Configuration (advanced)' Just press enter on 'Repair Your Computer.' Then Click on startup repair and if it asks for system restore click restore.");
 }
