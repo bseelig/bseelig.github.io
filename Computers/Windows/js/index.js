64f580006c12d8cc4951bb453c922f296e362010
bseelig365code <bseelig@me.com>
2018-01-25 20:34:06 -0500

    Edit steps on topics

--- Computers/Windows/js/index.js
+++ Computers/Windows/js/index.js
@@ -1,3 +1,3 @@
 function go() {
-  alert("Turn on your computer and keep pressing the F8 button rapidly until you see a menu. Press enter on repair your computer then click on the system restore and follow the steps on the screen.");
+  alert("Turn on or restart your computer and keep pressing the F8 button rapidly until you see a menu. Press enter on repair your computer then click on the system restore and follow the steps on the screen.");
 }
--- Computers/Windows/startup/js/index.js
+++ Computers/Windows/startup/js/index.js
@@ -1,30 +1,29 @@
 var answer;
 
 function bs() {
-  alert('Remember: OK = Yes and Cancel = No. Click on the Cancel button a few times to exit.');
-  var answer = confirm("First turn on your computer and hold the F2 key. This only works on computers that are or used to run Windows 7 or below. Did it enter into the Bios settings?");
+  var answer = confirm("First identify what your computer manufacturer is. Here are examples: HP, Dell, and Acer. On certain computers, there is a specific key that you need to press to open the BIOS or UEFI settings. This setting lets you adjust your computer to work a certain way. On most computers such as Dell and Acer, you press the F2 key or Delete (Not Backspace) key. On HP computers, you press the F10 key. To start, you need to restart or start your computer by pressing one of those keys depending on what manufacturer your computer is. Remember that these keys are on the very top of the keyboard. If this does not work, click the Cancel for other solutions. If it does work, click the OK button.");
 if( answer == true ){
-   alert("Go to the Advanced section then look for Stata Configuration. Then change the Stata Mode to a differnt setting. If you cannot find it, look all around the bios settings. Then exit saving changes and try booting up again.");
+   alert("On a BIOS or UEFI computer, use the keyboard arrows to navigate and press enter to select. If you see a cursor, use the mouse to navigate and click to select. Open the Advanced tab where you see the text Advanced then look for SATA Configuration. Change the SATA Mode from its current setting to a different setting. If you cannot find it, look all around the bios settings using all the tabs or sections. To exit, use the escape key on the top left. Make sure that you save changes before exiting. You are now finished, click OK to exit.");
 }
 else {
-  var answer = confirm("Hold the power button until the computer powers off. Now press the power button and then hold the delete key (Not the backspace key.) Did it go into the Bios settings?")
+  var answer = confirm("If you do not know how to open the BIOS or UEFI settings, look around the screen right when you turn it on. There is a key that you need to press that should show you how to open a setting. If there isn't, press the f keys from f1-f12 including the delete key. If this does not work, click the Cancel button for other solutions. If it does work, click the OK button.")
 if( answer == true ){
-   alert("Go to the Advanced section then look for Stata Configuration. Then change the Stata Mode to a differnt setting. If you cannot find it, look all around the bios settings. Then exit saving changes and try booting up again.");
+   alert("On a BIOS or UEFI computer, use the keyboard arrows to navigate and press enter to select. If you see a cursor, use the mouse to navigate and click to select. Open the Advanced tab where you see the text Advanced then look for SATA Configuration. Change the SATA Mode from its current setting to a different setting. If you cannot find it, look all around the bios settings using all the tabs or sections. To exit, use the escape key on the top left. Make sure that you save changes before exiting. You are now finished, click OK to exit.");
 }
   else{
-    alert("Hold the power button until the computer powers off. Then press the power button to turn it on. When you see the screen popup, look around for a text ('menu' or 'setup') and then press and hold on that key to go to the setup. If the screen disappers to fast, use a video camra or phone camra to replay. Pause at the screen right when it turns on. (To start this, restart to step one on this text).  If its a menu, press on the 'F (number)' key (On the top of the keybord) on the text 'BIOS Settings' or 'BIOS Setup'");
-    alert("Go to the Advanced section then look for Stata Configuration. Then change the Stata Mode to a differnt setting. If you cannot find it, look all around the bios settings. Then exit saving changes and try booting up again.");
+    alert("On Windows 8 and 10 you need to enter the troubleshooting menu. When you first turn on the computer, the Windows logo shows up. Press and hold the power button until the computer turns off. Repeat this step one more time after you turn it back on. When finished, you will enter in a troubleshooting menu. Use the mouse to click on the /Troubleshoot/Advanced options/UEFI Firmware Settings. Your computer should automatically restart into the setting. Click Ok to continue");
+    alert("On a BIOS or UEFI computer, use the keyboard arrows to navigate and press enter to select. If you see a cursor, use the mouse to navigate and click to select. Open the Advanced tab where you see the text Advanced then look for SATA Configuration. Change the SATA Mode from its current setting to a different setting. If you cannot find it, look all around the bios settings using all the tabs or sections. To exit, use the escape key on the top left. Make sure that you save changes before exiting. You are now finished, click OK to exit.");
   }
 }
 }
 
 function em() {
-  var answer = confirm("You will need a Windows Installer CD and a flash drive inorder to fix your problem. (This is for Windows 7) If not, you will lose all data and cannot continue the steps.");
+  var answer = confirm("You will need a Windows Installer CD and a flash drive inorder to fix your problem. If not, you will lose all data and cannot continue the steps.");
   
   if( answer == true ){
-   alert("Turn on your computer. Then boot into the CD. Select you language and then click Repair your computer. If its not booting into CD, Hold the power button until the computer powers off and then turn it back on. Hold the F12 key for the bootup menu use the keybord to move highlighted text and press enter to select.");
-    alert("Now click the next step and you will see repairing options. Click on the System Image Recovery. Error message will come up. Click Cancel then  Next then Advanced then Install a driver then OK. Dobble Click on the computer. Then Plug in your flash drive.");
-    alert("Now click on the Local Disk and then click users and coppy all your data into the flash drive. Big files must be coppyed separately in one coppy. To enter into your flash drive, Click computer then your flash drive. You must right click to coppy and paste.");
+   alert("Turn on your computer. Then boot into the CD. Select you language and then click Repair your computer. If its not booting into CD, Hold the power button until the computer powers off and then turn it back on. Hold the F12 key for the bootup menu use the keybord to move highlighted text and press enter to select. Click Ok to continue.");
+    alert("Now click the next step and you will see repairing options. Click on the System Image Recovery. Error message will come up. Click Cancel then  Next then Advanced then Install a driver then OK. Dobble Click on the computer. Then Plug in your flash drive. Click Ok to continue");
+    alert("Now click on the Local Disk and then click users and coppy all your data into the flash drive. Big files must be coppyed separately in one coppy. To enter into your flash drive, Click computer then your flash drive. You must right click to coppy and paste. Click Ok to continue");
   alert("When you are done, Reinstall Windows.");
   }
   
@@ -50,6 +49,6 @@ else {
 }
 
 function ss() {
-  alert("Hold the power button until the computer turns off. Then turn it back on. Keep pressing the F8 button rapidly until you see a menu. Use the keybord arrows to move the highlighted text. Move the highlight text to the 'Last Known Good Configuration (advanced).' Then press enter");
+  alert("Hold the power button until the computer turns off. Then turn it back on. Keep pressing the F8 button rapidly until you see a menu. Use the keybord arrows to move the highlighted text. Move the highlight text to the 'Last Known Good Configuration (advanced).' Then press enter. Wait for at least 5 mins before turning it off. Click Ok to continue.");
   alert("If it dose not work restart to step one but instead of doing 'Last Known Good Configuration (advanced)' Just press enter on 'Repair Your Computer.' Then Click on startup repair and if it asks for system restore click restore.");
 }
